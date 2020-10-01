cd782044f49379b76f973cdf2cfe158796222b44
Yuri <Yuribtr@users.noreply.github.com>
2020-10-02 00:23:08 +0300

    Made some refactoring to improve performance

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 import xml.etree.ElementTree as ET
+from time import perf_counter
+
+start_time = perf_counter()
 
 
-def get_top_words(text, min_len=6, words_qty=10):
-    words = set(text.split())
+def get_top_words(words, min_len=6, words_qty=10):
     result = [x for x in words if len(x) > min_len]
     result.sort(key=lambda x: len(x))
     return result[:-(words_qty + 1):-1]
@@ -13,9 +15,9 @@ def parse_json(filename, min_len=6, words_qty=10):
     with open(filename, encoding='utf-8') as file:
         file = json.load(file)
         items = file['rss']['channel']['items']
-        text = ''
+        text = set()
         for item in items:
-            text += item['description']
+            text.update(item['description'].split())
     return get_top_words(text, min_len, words_qty)
 
 
@@ -24,9 +26,9 @@ def parse_xml(filename, min_len=6, words_qty=10):
     tree = ET.parse(filename, parser)
     root = tree.getroot()
     items = root.findall('channel/item')
-    text = ''
+    text = set()
     for item in items:
-        text += item.find('description').text
+        text.update(item.find('description').text.split())
     return get_top_words(text, min_len, words_qty)
 
 
@@ -34,3 +36,6 @@ print('Вывод длинных слов из "newsafr.json":')
 print(*parse_json('newsafr.json'), sep='\n')
 print('\nВывод длинных слов из "newsafr.xml":')
 print(*parse_xml('newsafr.xml'), sep='\n')
+
+end_time = perf_counter()
+print(f"Time taken: {(end_time - start_time) * 1000000} mkS")
